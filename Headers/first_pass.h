d2cc5fa3222b375aaa4294de64747fbadb3a18b2
noam <noamshirm@gmail.com>
2025-08-03 18:02:40 +0300

    add: first_pass.h and mcro_table.h comments

--- Headers/first_pass.h
+++ Headers/first_pass.h
@@ -1,4 +1,12 @@
 #ifndef FIRST_PASS_H
 #define FIRST_PASS_H
+/**
+ * @brief Assembler first pass.
+ *
+ * This function reads line by line from the file received as param
+ * and decides its type (guide line, command line) and handles it.
+ *
+ * @param char* file_name_without_postfix A pointer to the file name (without the .am postfix).
+ */
 void first_pass(char *file_name_without_postfix);
 #endif
--- Headers/mcro_table.h
+++ Headers/mcro_table.h
@@ -7,8 +7,33 @@ typedef struct mcro_item
     struct mcro_item *next;
 } mcro_item;
 
+/**
+ * @brief create mcro item
+ * create a new mcro item with given name and value.
+ * @param name mcro item name (the mcro declaration name)
+ * @param value mcro item value (the mcro declaration content)
+ */
 mcro_item *create_item(char *name, char *value);
+/**
+ * @brief adds mcro item to table
+ * create a new mcro item with given name and value and adds it to the end of the list.
+ * @param head table head pointer
+ * @param name mcro item name (the mcro declaration name)
+ * @param value mcro item value (the mcro declaration content)
+ */
 mcro_item *add_item(mcro_item **head, char *name, char *value);
+/**
+ * @brief concat value to mcro item value
+ * Appends string (value) to the mcro item value received as prop.
+ * @param item mcro item
+ * @param value string to add to the mcro item's value
+ */
 void append_item_value(mcro_item *item, char *value);
+/**
+ * @brief finds mcro item by name
+ * search the table for a mcro item with the same name as the one received as prop.
+ * @param head table head pointer
+ * @param name the name we want to search
+ */
 mcro_item *find_by_name(mcro_item *head, char *name);
 #endif
