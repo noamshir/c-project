6f82860f8d870fcd182a0782354cf0cf4430d789
EliRannn <eliranbehor@gmail.com>
2025-08-04 13:25:30 +0300

    fix for secPass h

--- Headers/second_pass.h
+++ Headers/second_pass.h
@@ -2,6 +2,5 @@
 #define SECOND_PASS_H
 
 void second_pass(char *file_name_without_postfix, symbol_item *symbol_table, char **array_of_commands, int IC, char **array_of_data, int DC);
-int encode_second_pass_operands(symbol_item *symbol_table, char *line, char **array_of_commands,char ***externLabels, int **externAddresses, int *externCount);
-
+int encode_second_pass_operands(char *op, int type, int space, int *IC, char ***array_of_commands,symbol_item *symbol_table, char ***externLabels, int **externAddresses, int *externCount);
 #endif
